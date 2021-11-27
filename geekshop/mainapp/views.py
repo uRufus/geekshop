50a6c80efdddf4c85ce5719139c7356085830ef6
Rufus <apdubrov@gmail.com>
2021-11-27 12:28:54 +0300

    lesson_4

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -6,17 +6,18 @@ from .models import ProductCategory, Product
 
 
 def index(request):
-    content = {
+    context = {
         'title': 'geekshop',
     }
-    return render(request, 'mainapp/index.html', content)
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    content = {
+    context = {
         'title': 'geekshop - каталог',
     }
-    content['products'] = Product.objects.all()
-    content['vendor_slides'] = load(open('mainapp/fixtures/vendor_slides.json', encoding='utf-8'))
+    context['products'] = Product.objects.all()
+    context['categories'] = ProductCategory.objects.all()
+    context['vendor_slides'] = load(open('mainapp/fixtures/vendor_slides.json', encoding='utf-8'))
     # content = load(open('mainapp/fixtures/products.json', encoding='utf-8'))
-    return render(request, 'mainapp/products.html', content)
+    return render(request, 'mainapp/products.html', context)
