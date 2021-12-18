f65e5f4de4e0122d141820b6d60c096414ede40c
Rufus <apdubrov@gmail.com>
2021-12-18 22:03:27 +0300

    homework 1

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
+
+    path('verify/<str:email>/<str:activate_key>', UserRegisterView.verify, name='verify'),
 ]
