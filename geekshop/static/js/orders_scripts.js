a58455d72acac466ad503045588c84823f3b2a03
Rufus <apdubrov@gmail.com>
2022-01-04 13:33:41 +0300

    update orders js script

--- geekshop/static/js/orders_scripts.js
+++ geekshop/static/js/orders_scripts.js
@@ -73,7 +73,7 @@ window.onload = function () {
         orderSummaryUpdate(price_arr[orderitem_num], delta_quantity)
     }
 
-     $('.order_form select').change(function () {
+    $('.order_form select').change(function () {
 
         let target = event.target;
         orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
@@ -103,5 +103,37 @@ window.onload = function () {
         }
 
     })
-}
 
+    // $('.order_form select').change(function () {
+    $(document).on('change', '.order_form select', function () {
+
+        let target = event.target;
+        orderitem_num = parseInt(target.name.replace('orderitems-', '').replace('-product', ''));
+        let orderitem_product_pk = target.options[target.selectedIndex].value;
+
+        console.log(orderitem_num)
+        console.log(orderitem_product_pk)
+
+        if (orderitem_product_pk) {
+            $.ajax({
+                url: '/orders/product/' + orderitem_product_pk + '/price/',
+                success: function (data) {
+                    if (data.price) {
+                        price_arr[orderitem_num] = parseFloat(data.price)
+                        if (isNaN(quantity_arr[orderitem_num])) {
+                            quantity_arr[orderitem_num] = 0;
+                        }
+                        let price_html = '<span class="orderitems-' + orderitem_num + '-price">'
+                            + data.price.toString().replace('.', ',') + '</span> руб';
+                        let current_tr = $('.order_form table').find('tr:eq(' + (orderitem_num + 1) + ')');
+                        current_tr.find('td:eq(2)').html(price_html)
+                    }
+                }
+
+
+            })
+        }
+
+    })
+
+}
